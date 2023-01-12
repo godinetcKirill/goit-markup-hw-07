0309190e57f2d91efd700a6ad9e32265dcd3d483
godinetcKirill <117347199+godinetcKirill@users.noreply.github.com>
2023-01-12 16:39:42 +0300

    error js

---
+++ mobile-menu.js
@@ -0,0 +1,8 @@
+openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+  // Закрываем мобильное меню на более широких экранах
+  // в случае изменения ориентации устройства.
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
